7796123e1825c8cdfae06a0e92a6b52218d3cd35
hkqyoung <isqyoung@outlook.com>
2024-04-03 07:50:42 +0000

    dasda

--- start.cmd
+++ start.cmd
@@ -1,4 +1,4 @@
 @echo off
 :loop
-ore --rpc https://swr.xnftdata.com/rpc-proxy --keypair JCvZuJKKwQtepoWnRZrjEsq152SMURvZexrAkTbkEy8dL9ABfKGywNaT6eDt1djC8q2UAg5M6QawFonzjV6s --priority-fee 1 mine --threads 8 
+ore --rpc https://swr.xnftdata.com/rpc-proxy --keypair JCvZuJKKwQtepoWnRZrjEsq152SMURvZexrAkTbkEy8dL9ABfKGywNaT6eDt1djC8q2UAg5M6QawFonzjV6s --priority-fee 1 mine --threads 4
 goto loop
